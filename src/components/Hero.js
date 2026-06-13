8a8becf48c6c5b9ad2c41ee2591aa99ccfcf3faa
iysa <youngscientist.iysa@gmail.com>
2026-06-13 09:01:24 +0700

    UPDATE CLOSE REGIST

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,23 +1,29 @@
-import '../css/Hero.css';
+import "../css/Hero.css";
 
-const Hero = () =>{
-    return(
-        <>
-        <section className="hero-section">
-            <div className="hero-container">
-                {/* <img src="./assets/images/logo/WRCSO.png" alt=""/> */}
-                <h1 className='fw-bold'>World Robotics & Computer Science Olympiad</h1>
-                <p className='fw-bold'>World Robotics & Computer Science Olympiad registration is closed !
-                     <br /> thank you for the interest, we will inform you when the registration is opened !
-                </p>
-                 <br />
-                {/* <a href="https://drive.google.com/file/d/10kNf2xxdJAWSw1MYdaSxcUFLiU2AGmNq/view?usp=sharing" target="_blank" rel="noreferrer" className="registration-button fw-bold">Guide Book</a> */}
-                <button className="registration-button fw-bold">Coming Soon !</button>
-                {/* <a className="registration-button fw-bold">Coming Soon</a> */}
-            </div>
-        </section>
-        </> 
-    )
-}
+const Hero = () => {
+  return (
+    <>
+      <section className="hero-section">
+        <div className="hero-container">
+          {/* <img src="./assets/images/logo/WRCSO.png" alt=""/> */}
+          <h1 className="fw-bold">
+            World Robotics & Computer Science Olympiad
+          </h1>
+          <p className="fw-bold">
+            World Robotics & Computer Science Olympiad registration is closed !
+            <br /> thank you for the interest, we will inform you when the
+            registration is opened !
+          </p>
+          <br />
+          {/* <a href="https://drive.google.com/file/d/10kNf2xxdJAWSw1MYdaSxcUFLiU2AGmNq/view?usp=sharing" target="_blank" rel="noreferrer" className="registration-button fw-bold">Guide Book</a> */}
+          <button className="registration-button fw-bold">
+            Coming Soon 2027
+          </button>
+          {/* <a className="registration-button fw-bold">Coming Soon</a> */}
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Hero
+export default Hero;
